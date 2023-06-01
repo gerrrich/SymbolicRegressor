bff988a11d73bad9666dd0765394743baa0ac396
gerrrich <gerrich@my.com>
2023-06-01 21:42:44 +0300

    text

--- mec.py
+++ mec.py
@@ -1,35 +1,9 @@
 import math
 import random
+from typing import List
 
 import numpy as np
-
-
-class Omega:
-    omega_changed = False
-    new_omega = 0.0
-
-    def __init__(self):
-        self.omega = 0.0
-        self.next_number = 0.0
-        self.if_next_number = False
-
-    def new_number_with_omega(self):
-        if self.if_next_number:
-            self.if_next_number = False
-            return self.next_number * self.omega
-        else:
-            s, x, y = 0, None, None
-
-            while s >= 1 or s == 0:
-                x = 2 * random.random() - 1
-                y = 2 * random.random() - 1
-                s = x * x + y * y
-
-            part = math.sqrt(-2 * math.log(s, math.e) / s)
-            self.next_number = y * part
-            self.if_next_number = True
-
-            return x * part * self.omega
+import matplotlib.pyplot as plt
 
 
 class Billboard:
@@ -39,69 +13,20 @@ class Billboard:
 
 
 class Population:
-    def __init__(self, func, dimension):
-        self.omega = Omega()
-        self._SGC, self._TGC, self._IC = np.uint32(), np.uint32(), np.uint32()
-        self._xmaxs = None
-        self.Xmins = None
+    def __init__(self, func, dimension, SGC, TGC, IC, Xmaxs, Xmins, omega):
+        self.omega = omega
+        self.SGC, self.TGC, self.IC = SGC, TGC, IC
+        self.Xmaxs = Xmaxs
+        self.Xmins = Xmins
         self.func = func
         self.dimension = dimension
 
-    @property
-    def SGC(self):
-        return self._SGC
-
-    @SGC.setter
-    def SGC(self, value):
-        if value == 0:
-            raise Exception('Error')
-        self._SGC = value
-
-    @property
-    def TGC(self):
-        return self._TGC
-
-    @TGC.setter
-    def TGC(self, value):
-        if value == 0:
-            raise Exception('Error')
-        self._TGC = value
-
-    @property
-    def IC(self):
-        return self._IC
-
-    @IC.setter
-    def IC(self, value):
-        if value == 0:
-            raise Exception('Error')
-        self._IC = value
-
-    @property
-    def Xmaxs(self):
-        return self._xmaxs
-
-    @Xmaxs.setter
-    def Xmaxs(self, value):
-        for v in value:
-            if v < self._xmaxs:
-                raise Exception('Error')
-        self._xmaxs = value
-
-    def sides(self, x, dimension_number: int):
-        if x > self.Xmaxs[dimension_number]:
-            x = self.Xmaxs[dimension_number]
-        elif x < self.Xmins[dimension_number]:
-            x = self.Xmins[dimension_number]
-        return x
-
     def initialization(self):
-        group = np.ndarray(shape=(self.IC, self.dimension - 1), dtype=np.float)
-        group[0] = self.Xmins + (self.Xmaxs - self.Xmins) * np.random.uniform(size=(self.dimension - 1))
+        group = np.ndarray(shape=(self.IC, self.dimension - 1), dtype='float32')
+        group[0, :] = np.random.uniform(self.Xmins, self.Xmaxs, size=(self.dimension - 1))
 
         for i in range(1, group.shape[0]):
-            for j in range(group.shape[1]):
-                group[i, j] = self.sides(group[0, j] + self.omega.new_number_with_omega(), j)
+            group[i, :] = group[0, :] + np.random.normal(0, self.omega, group.shape[1])
 
         return group
 
@@ -126,14 +51,13 @@ class Population:
 
         return local
 
-    def similar_taxis(self, glob: list[Billboard], sg: np.ndarray, sloc: list[list[Billboard]], tg: np.ndarray, tloc: list[list[Billboard]]):
+    def similar_taxis(self, glob: List[Billboard], sg: np.ndarray, sloc: List[List[Billboard]], tg: np.ndarray, tloc: List[List[Billboard]]):
         def temp(g: np.ndarray, loc, t):
             for i in range(g.shape[0]):
                 g[i, 0, :] = g[i, loc[i][0].index, :]
 
                 for k in range(1, g.shape[1]):
-                    for j in range(g.shape[2]):
-                        g[i, k, j] = self.sides(g[i, 0, j] + self.omega.new_number_with_omega(), j)
+                    g[i, k, :] = g[i, 0, :] + np.random.normal(0, self.omega, g.shape[2])
 
                 loc[i] = self.form_local_billboard(g[i])
                 glob[i + t].score = loc[i][0].score
@@ -141,7 +65,7 @@ class Population:
         temp(sg, sloc, 0)
         temp(tg, tloc, sg.shape[0])
 
-    def dissimilation(self, glob: list[Billboard], sg: np.ndarray, sloc: list[list[Billboard]], tg: np.ndarray, tloc: list[list[Billboard]]):
+    def dissimilation(self, glob: List[Billboard], sg: np.ndarray, sloc: List[List[Billboard]], tg: np.ndarray, tloc: List[List[Billboard]]):
         for i in range(sg.shape[0]):
             for j in range(sg.shape[0], len(glob)):
                 if glob[j].score > glob[i].score:
@@ -154,17 +78,77 @@ class Population:
                         sloc[glob[i].index] = self.form_local_billboard(sg[glob[i].index])
                         glob[i].score = sloc[glob[i].index][0].score
 
-    def find_max(self, gbb: list[Billboard]):
+    def find_max(self, gbb: List[Billboard]):
         return max(gbb, key=lambda x: x.score).score
 
-    def find_arg(self, gbb: list[Billboard], sbb: list[list[Billboard]], sg: list[list]):
+    def find_arg(self, gbb: List[Billboard], sbb: List[List[Billboard]], sg: List[list]):
         index = max(gbb, key=lambda x: x.score).index
         return sg[index][sbb[index][0].index]
 
-    def form_global_billboard(self, sloc: list[list[Billboard]], tloc: list[list[Billboard]]):
+    def form_global_billboard(self, sloc: List[List[Billboard]], tloc: List[List[Billboard]]):
         glob = [Billboard(sloc[i][0].score, i) for i in range(len(sloc))]
 
         for i in range(len(sloc), len(sloc) + len(tloc)):
             glob.append(Billboard(tloc[i - len(sloc)][0].score, i - len(sloc)))
 
         return glob
+
+
+def work(population):
+    try:
+        SG = np.ndarray(shape=(population.SGC, population.IC, population.dimension - 1), dtype='float32')
+        TG = np.ndarray(shape=(population.TGC, population.IC, population.dimension - 1), dtype='float32')
+        SGLBB = []
+        TGLBB = []
+
+        for i in range(SG.shape[0]):
+            SG[i, :, :] = population.initialization()
+            SGLBB.append(population.form_local_billboard(SG[i, :, :]))
+
+        for i in range(TG.shape[0]):
+            TG[i, :, :] = population.initialization()
+            TGLBB.append(population.form_local_billboard(TG[i, :, :]))
+
+        GLBB = population.form_global_billboard(SGLBB, TGLBB)
+
+        old_score = population.find_max(GLBB)
+        new_score = float('inf')
+        count = 0
+
+        while new_score > old_score:
+            if count > 0:
+                old_score = new_score
+
+            population.similar_taxis(GLBB, SG, SGLBB, TG, TGLBB)
+            population.dissimilation(GLBB, SG, SGLBB, TG, TGLBB)
+
+            new_score = population.find_max(GLBB)
+            count += 1
+
+        if np.isnan(new_score) or np.isinf(new_score):
+            print('В функции был найден разрыв. Проверьте область определения. Ошибка')
+            return None
+        else:
+            x = population.find_arg(GLBB, SGLBB, SG)
+            print(f'Максимум функции: {new_score} в точке: {x}')
+            print(f'Количество итераций: {count}')
+
+
+            return (x, new_score), count
+
+    except MemoryError:
+        print('Популяция слишком велика. Произошло переполнение памяти.')
+
+
+f = lambda x1, x2: x1 * np.sin(x2)
+
+population = Population(f, 3, 10, 10, 10, np.array([10, 10]), np.array([-10, -10]), 0.1)
+
+point, count = work(population)
+
+x1 = np.linspace(-10, 10, 200)
+x2 = np.linspace(-10, 10, 200)
+# plt.plot(x, f(x), 'r')
+# plt.plot(point[0],point[1], 'oy')
+plt.
+plt.show()
